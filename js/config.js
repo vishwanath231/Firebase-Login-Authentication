283204fba863383e4d5a7e6e18061ef731f80859
Vishwanath <vishwanathvishwabai@gmail.com>
2021-09-02 20:41:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,6 @@
 // Your web app's Firebase configuration
         var firebaseConfig = {
-            apiKey: "AIzaSyBs9gIH9eAHIJ4liXl7zJFsKYzP2PSrF14",
-            authDomain: "basicform-5b712.firebaseapp.com",
-            databaseURL: "https://basicform-5b712-default-rtdb.firebaseio.com",
-            projectId: "basicform-5b712",
-            storageBucket: "basicform-5b712.appspot.com",
-            messagingSenderId: "672972270504",
-            appId: "1:672972270504:web:01d3f6857d5dfc001dcb9d"
+            // firebase Config
         };
 
         // Initialize Firebase
